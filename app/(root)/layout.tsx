15d6ae02da341dff836505465eba2cc322b6ce93
PioKl <kosiu342@gmail.com>
2025-08-22 17:50:02 +0200

    Add layout update (Header, layout styles)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,14 @@
+import Header from "@/components/shared/header/Header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <header></header>
-      <main>{children}</main>
-      <footer></footer>
+    <div className="flex flex-col-reverse lg:flex-row">
+      <Header />
+      <main className="">{children}</main>
     </div>
   );
 }
