fa2aad475d590eada23aa9d5964380c58edd6d99
PioKl <kosiu342@gmail.com>
2025-08-26 16:46:22 +0200

    Add types

---
+++ types/index.tsx
@@ -0,0 +1,6 @@
+import { LinkButton, StandardButton } from "@/interfaces";
+
+export type ButtonProps<T> =
+  | (StandardButton<T> & React.ButtonHTMLAttributes<HTMLButtonElement>) //Zwykły button
+  | (LinkButton<T> &
+      Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">); //Link, bez href bo nie chcę href przekazywać gdy jest anchor tylko przy pomocy link, chcę żebym mógł przekazać href we właściowości link jeśli isALink jest true
