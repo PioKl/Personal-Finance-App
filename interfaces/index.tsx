26d9569180d8ee18d039b9499951837252257076
PioKl <kosiu342@gmail.com>
2025-08-27 18:50:11 +0200

    interfaces update

--- interfaces/index.tsx
+++ interfaces/index.tsx
@@ -1,24 +1,20 @@
 export interface Button<T> {
   children: React.ReactNode;
-  variant:
-    | "primary"
-    | "secondary"
-    | "tertiary"
-    | "destroy"
-    | "sidebar"
-    | "link";
   isALink?: boolean;
   onClick?: () => void;
   className?: string;
+  icon?: React.ElementType;
   state?: T;
 }
 
 export interface LinkButton<T> extends Button<T> {
   isALink: true;
   link: string;
+  variant: "link";
 }
 
 export interface StandardButton<T> extends Button<T> {
   isALink?: false;
   link?: never;
+  variant: "primary" | "secondary" | "tertiary" | "destroy" | "sidebar";
 }
