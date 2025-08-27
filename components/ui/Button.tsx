98628f12fad62f3cbe7591a6d7ed01502c931d00
PioKl <kosiu342@gmail.com>
2025-08-27 18:51:37 +0200

    Button update

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -4,23 +4,28 @@ import clsx from "clsx";
 
 const Button = <T,>({
   children,
-  variant = "primary",
+  variant,
   isALink,
   link,
   onClick,
   className,
+  icon: Icon,
+  state,
 }: ButtonProps<T>) => {
   const variantStyles: Record<
     NonNullable<ButtonProps<T>["variant"]>,
     string
   > = {
+    //Standard Button
     primary: "",
     secondary: "",
     tertiary: "",
     destroy: "",
+    //Sidebar
     sidebar:
       "group items-end text-left text-heading-m tracking-heading-m leading-heading-m font-heading-m text-button hidden lg:flex lg:mt-auto lg:ml-space-400 lg:mb-14.5",
-    link: "flex flex-col flex-1 items-center w-fit pt-space-100 pb-space-150 px-5.5 font-heading-xs-bold border-b-4 rounded-t-menu-mobile md:px-2.5 lg:flex-row lg:w-full lg:py-space-200 lg:px-space-400 lg:border-b-0 lg:border-l-4 lg:rounded-t-none lg:rounded-r-menu",
+    //Link
+    link: "",
   };
 
   return isALink ? (
@@ -32,7 +37,24 @@ const Button = <T,>({
       className={clsx(variantStyles[variant], className)}
       onClick={onClick}
     >
-      {children}
+      {variant === "sidebar" ? (
+        <>
+          {Icon && (
+            <Icon
+              className={`${
+                state ? "mr-0 ml-space-50 rotate-180" : "mr-space-200 rotate-0"
+              } group-hover:[&>path]:fill-button-active group-focus-visible:[&>path]:fill-button-active`}
+            />
+          )}
+          {!state && (
+            <span className="group-hover:text-button-active group-focus-visible:text-button-active">
+              {children}
+            </span>
+          )}
+        </>
+      ) : (
+        children
+      )}
     </button>
   );
 };
