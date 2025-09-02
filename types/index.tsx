214e81474d48659f3b89f5319ae58ecbed8f96d8
PioKl <kosiu342@gmail.com>
2025-09-02 20:06:26 +0200

    Types update

--- types/index.tsx
+++ types/index.tsx
@@ -4,3 +4,9 @@ export type ButtonProps<T> =
   | (StandardButton<T> & React.ButtonHTMLAttributes<HTMLButtonElement>) //Zwykły button
   | (LinkButton<T> &
       Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">); //Link, bez href bo nie chcę href przekazywać gdy jest anchor tylko przy pomocy link, chcę żebym mógł przekazać href we właściowości link jeśli isALink jest true
+
+export type Balance = {
+  current: number;
+  income: number;
+  expenses: number;
+};
