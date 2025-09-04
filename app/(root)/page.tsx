42617dc5161de8474ae2dc04583cf3b4f604f4c1
PioKl <kosiu342@gmail.com>
2025-09-04 21:33:19 +0200

    Pots in Overview

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,27 @@
 import data from "@/data/data.json";
-import { Balance } from "@/types";
+import { Balance, Pots } from "@/types";
 import { priceDollarsFormatting } from "@/utils/formattingFunctions";
+import Button from "@/components/ui/Button";
+import IconCaretRight from "@/assets/icons/icon-caret-right.svg";
+import IconPot from "@/assets/icons/icon-pot.svg";
 
 export const metadata = {
   title: "Overview",
 };
 
 export default function Home() {
+  //const { balance, pots } = data as { balance: Balance; pots: Pots[] };
   const { balance } = data as { balance: Balance };
+  const { pots } = data as { pots: Pots[] };
+  const potsSlice = pots.slice(0, 4);
+  console.log(potsSlice);
 
   return (
     <>
       <div className="mb-space-400">
         <h1>Overview</h1>
       </div>
-      <div className="flex flex-col gap-space-150 md:flex-row md:gap-space-300">
+      <div className="flex flex-col gap-space-150 mb-space-400 md:flex-row md:gap-space-300">
         {Object.entries(balance).map(([key, value], index) => (
           <div
             key={key}
@@ -32,6 +39,40 @@ export default function Home() {
             </span>
           </div>
         ))}
+      </div>
+      <div>
+        <div className="grid gap-space-250 py-space-300 px-space-250 md:p-space-400 bg-fill-two rounded-default">
+          <div className="flex items-end justify-between">
+            <h2>Pots</h2>
+            <Button
+              variant="link"
+              isALink={true}
+              link="#"
+              className="flex items-center gap-space-150"
+            >
+              See Details
+              <IconCaretRight />
+            </Button>
+          </div>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex items-center gap-4 py-5 px-4 bg-fill-three rounded-default">
+              <IconPot />
+              <div className="grid gap-3">
+                <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4 text-color-three">
+                  Total Saved
+                </span>
+                <span className="text-preset-1 tracking-preset-1 leading-preset-1 font-preset-1 text-color-one">
+                  $850
+                </span>
+              </div>
+            </div>
+            <ul>
+              {potsSlice.map((item, index) => (
+                <li key={index}>{item.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
