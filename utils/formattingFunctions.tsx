51686ab484dd3aaa7cc9cba1fafaa24b6b51f42b
PioKl <kosiu342@gmail.com>
2025-09-09 18:26:05 +0200

    Add new formatting functions

--- utils/formattingFunctions.tsx
+++ utils/formattingFunctions.tsx
@@ -1,6 +1,40 @@
+//Dollar Format
+
 export const priceDollarsFormatting = (price: number): string => {
   return price.toLocaleString("en-US", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
 };
+
+//Date Format
+
+export function formatData(dateString: string): string {
+  return new Date(dateString).toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+}
+
+export function formatDateTime(dateString: string): string {
+  return new Date(dateString).toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+}
+
+export function formatAmount(amount: number): string {
+  return (
+    (amount >= 0 ? "+" : "") +
+    amount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    })
+  );
+}
