49aaecc69eb510fc8398a45121770c2a1e7eac46
PioKl <kosiu342@gmail.com>
2025-09-09 18:26:53 +0200

    Add new type

--- types/index.tsx
+++ types/index.tsx
@@ -17,3 +17,12 @@ export type Pots = {
   total: number;
   theme: string;
 };
+
+export type Transactions = {
+  avatar: string;
+  name: string;
+  category: string;
+  date: string;
+  amount: number;
+  recurring: boolean;
+};
