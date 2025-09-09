a389ed181e51e2a7e582056a7e359b634c82a735
PioKl <kosiu342@gmail.com>
2025-09-09 18:28:10 +0200

    Overview page update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
 import data from "@/data/data.json";
-import { Balance, Pots } from "@/types";
-import { priceDollarsFormatting } from "@/utils/formattingFunctions";
+import Image from "next/image";
+import { Balance, Pots, Transactions } from "@/types";
+import {
+  priceDollarsFormatting,
+  formatData,
+  formatAmount,
+} from "@/utils/formattingFunctions";
 import Button from "@/components/ui/Button";
 import IconCaretRight from "@/assets/icons/icon-caret-right.svg";
 import IconPot from "@/assets/icons/icon-pot.svg";
@@ -13,79 +18,135 @@ export default function Home() {
   //const { balance, pots } = data as { balance: Balance; pots: Pots[] };
   const { balance } = data as { balance: Balance };
   const { pots } = data as { pots: Pots[] };
+  const { transactions } = data as { transactions: Transactions[] };
   const potsSlice = pots.slice(0, 4);
+  const transactionsSlice = transactions.slice(0, 5);
   console.log(potsSlice);
+  console.log(transactionsSlice);
 
   return (
     <>
-      <div className="mb-space-400">
-        <h1>Overview</h1>
-      </div>
-      <div className="flex flex-col gap-space-150 mb-space-400 md:flex-row md:gap-space-300">
-        {Object.entries(balance).map(([key, value], index) => (
-          <div
-            key={key}
-            className={`grid gap-space-150 flex-1 p-space-300 rounded-default first:bg-fill-one bg-fill-two first:text-color-two text-color-three`}
-          >
-            <span className="capitalize font-preset-4 text-preset-4 leading-preset-4 tracking-preset-4">
-              {key === "current" ? `${key} Balance` : key}
-            </span>
-            <span
-              className={`font-preset-1 text-preset-1 leading-preset-1 tracking-preset-1 ${
-                index === 0 ? "text-color-two" : "text-color-one"
-              }`}
+      <section className="grid gap-space-400">
+        <div>
+          <h1>Overview</h1>
+        </div>
+        <div className="flex flex-col gap-space-150 md:flex-row md:gap-space-300">
+          {Object.entries(balance).map(([key, value], index) => (
+            <div
+              key={key}
+              className={`grid gap-space-150 flex-1 p-space-300 rounded-default first:bg-fill-one bg-fill-two first:text-color-two text-color-three`}
             >
-              ${priceDollarsFormatting(value)}
-            </span>
+              <span className="capitalize font-preset-4 text-preset-4 leading-preset-4 tracking-preset-4">
+                {key === "current" ? `${key} Balance` : key}
+              </span>
+              <span
+                className={`font-preset-1 text-preset-1 leading-preset-1 tracking-preset-1 ${
+                  index === 0 ? "text-color-two" : "text-color-one"
+                }`}
+              >
+                ${priceDollarsFormatting(value)}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div>
+          <div className="grid gap-space-250 py-space-300 px-space-250 bg-fill-two rounded-default md:p-space-400">
+            <div className="flex items-end justify-between">
+              <h2>Pots</h2>
+              <Button
+                variant="link"
+                isALink={true}
+                link="/pots"
+                className="flex items-center gap-space-150"
+              >
+                See Details
+                <IconCaretRight />
+              </Button>
+            </div>
+            <div className="flex flex-col gap-space-250 md:flex-row">
+              <div className="flex items-center gap-space-200 py-5 px-4 bg-fill-three rounded-default md:pr-22">
+                <IconPot />
+                <div className="grid gap-3">
+                  <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4 text-color-three">
+                    Total Saved
+                  </span>
+                  <span className="text-preset-1 tracking-preset-1 leading-preset-1 font-preset-1 text-color-one">
+                    $850
+                  </span>
+                </div>
+              </div>
+              <ul className="grid grid-cols-2 gap-space-200 md:flex-1">
+                {potsSlice.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex gap-space-200 [&:nth-child(3)]:col-start-2 [&:nth-child(3)]:row-start-1 before:content-[''] before:w-1 [&:nth-child(1)]:before:bg-pots-one [&:nth-child(2)]:before:bg-pots-three [&:nth-child(3)]:before:bg-pots-two [&:nth-child(4)]:before:bg-pots-four before:rounded-default"
+                  >
+                    <div className="grid gap-space-50">
+                      <span className="text-preset-5 tracking-preset-5 leading-preset-5 font-preset-5 text-color-three">
+                        {item.name}
+                      </span>
+                      <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4-bold text-color-one">
+                        ${item.total}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        ))}
-      </div>
-      <div>
-        <div className="grid gap-space-250 py-space-300 px-space-250 md:p-space-400 bg-fill-two rounded-default">
-          <div className="flex items-end justify-between">
-            <h2>Pots</h2>
+        </div>
+        <div className="flex flex-col gap-space-400 py-space-300 px-space-250 bg-fill-two rounded-default md:p-space-400">
+          <div className="flex w-full items-end justify-between">
+            <h2>Transactions</h2>
             <Button
               variant="link"
               isALink={true}
-              link="#"
+              link="/transactions"
               className="flex items-center gap-space-150"
             >
-              See Details
+              View All
               <IconCaretRight />
             </Button>
           </div>
-          <div className="flex flex-col gap-space-250 md:flex-row">
-            <div className="flex items-center gap-space-200 py-5 px-4 bg-fill-three rounded-default md:pr-22">
-              <IconPot />
-              <div className="grid gap-3">
-                <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4 text-color-three">
-                  Total Saved
-                </span>
-                <span className="text-preset-1 tracking-preset-1 leading-preset-1 font-preset-1 text-color-one">
-                  $850
-                </span>
-              </div>
-            </div>
-            <ul className="grid grid-cols-2 gap-space-200 md:flex-1">
-              {potsSlice.map((item, index) => (
-                <li
-                  key={index}
-                  className="flex gap-space-200 [&:nth-child(3)]:col-start-2 [&:nth-child(3)]:row-start-1 before:content-[''] before:w-1 [&:nth-child(1)]:before:bg-pots-one [&:nth-child(2)]:before:bg-pots-three [&:nth-child(3)]:before:bg-pots-two [&:nth-child(4)]:before:bg-pots-four before:rounded-default"
-                >
-                  <div className="grid gap-space-50">
-                    <span className="text-preset-5 tracking-preset-5 leading-preset-5 font-preset-5 text-color-three">
-                      {item.name}
-                    </span>
-                    <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4-bold text-color-one">
-                      ${item.total}
-                    </span>
+          <ul className="grid gap-space-250">
+            {transactionsSlice.map((item, index) => (
+              <li
+                key={index}
+                className="flex justify-between pb-space-250 border-b-1 border-default"
+              >
+                <div className="flex items-center gap-space-200">
+                  <div className="relative w-8 h-8 md:w-10 md:h-10">
+                    <Image
+                      src={item.avatar.replace("./", "/")}
+                      alt={item.name}
+                      fill
+                      className="rounded-full object-cover"
+                      sizes="(min-width: 768px) 40px, 32px"
+                    />
                   </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4-bold">
+                    {item.name}
+                  </span>
+                </div>
+
+                <div className="grid gap-space-100">
+                  <span
+                    className={`
+                      text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4-bold
+                      ${item.amount >= 0 ? "text-amount" : "text-amount-alt"}
+                    `}
+                  >
+                    {formatAmount(item.amount)}
+                  </span>
+                  <span className="text-preset-5 tracking-preset-5 leading-preset-5 font-preset-5 text-date">
+                    {formatData(item.date)}
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
+      </section>
     </>
   );
 }
