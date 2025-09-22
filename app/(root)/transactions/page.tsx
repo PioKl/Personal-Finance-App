341ed398fde2a6a50d5319be0d904a756a2f5797
PioKl <kosiu342@gmail.com>
2025-09-22 19:17:51 +0200

    Transactions page update

--- app/(root)/transactions/page.tsx
+++ app/(root)/transactions/page.tsx
@@ -1,5 +1,53 @@
+import data from "@/data/data.json";
+import type { Transactions } from "@/types";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { formatDate } from "@/utils/formattingFunctions";
+
 const Transactions = () => {
-  return <div>Transactions</div>;
+  const { transactions } = data as { transactions: Transactions[] };
+
+  return (
+    <section className="grid gap-space-400">
+      <div>
+        <h1>Transactions</h1>
+      </div>
+      <div className="flex flex-col gap-space-250 py-space-300 px-space-250 bg-fill-two rounded-default md:p-space-400 ">
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className="!p-space-200">
+                  Recipient / Sender
+                </TableCell>
+                <TableCell>Category</TableCell>
+                <TableCell>Transaction Date</TableCell>
+                <TableCell align="right">Amount</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {transactions.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className="!p-space-200">{item.name}</TableCell>
+                  <TableCell>{item.category}</TableCell>
+                  <TableCell>{formatDate(item.date)}</TableCell>
+                  <TableCell align="right">{item.amount.toFixed(2)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </section>
+  );
 };
 
 export default Transactions;
