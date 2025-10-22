a46c7660e21432606b98135a965a02c06094f9a5
PioKl <kosiu342@gmail.com>
2025-10-22 18:46:38 +0200

    Page update (sectioncard and budgetchart)

--- app/(root)/budgets/page.tsx
+++ app/(root)/budgets/page.tsx
@@ -1,5 +1,27 @@
+import data from "@/data/data.json";
+import { Budgets as BudgetsType } from "@/types";
+import BudgetChart from "@/components/shared/BudgetChart";
+import SectionCard from "@/components/shared/SectionCard";
 const Budgets = () => {
-  return <h1>Budgets</h1>;
+  const { budgets } = data as { budgets: BudgetsType[] };
+  return (
+    <section className="grid gap-space-400">
+      <div>
+        <h1>Budgets</h1>
+      </div>
+      <div>
+        <SectionCard
+          className="gap-space-400"
+          title="Budgets"
+          link="/budgets"
+          linkLabel="See Details"
+          variant="budgets"
+        >
+          <BudgetChart budgets={budgets} variant="budgets" />
+        </SectionCard>
+      </div>
+    </section>
+  );
 };
 
 export default Budgets;
