2bbc953def3e82ab7d95ead02bbc62691d6d1721
PioKl <kosiu342@gmail.com>
2025-10-27 19:29:10 +0100

    Interfaces update BudgetsWithTransactions

--- interfaces/index.tsx
+++ interfaces/index.tsx
@@ -1,3 +1,5 @@
+import { Budgets, Transactions } from "@/types";
+
 export interface Button<T> {
   children: React.ReactNode;
   isALink?: boolean;
@@ -17,4 +19,10 @@ export interface StandardButton<T> extends Button<T> {
   isALink?: false;
   link?: never;
   variant: "primary" | "secondary" | "tertiary" | "destroy" | "sidebar";
+}
+
+//Omit<Budgets, "_type">, żeby odziedziczyć wszystkie pola oprócz _type
+export interface BudgetsWithTransactions extends Omit<Budgets, "_type"> {
+  _type: "BudgetsWithTransactions"; //używane do rozróżnienia typów w unionach
+  transactions: Transactions[];
 }
