49503c15d17607b7652318ed78ec9fbe2d93ab17
PioKl <kosiu342@gmail.com>
2025-11-19 19:24:00 +0100

    Add themes to utils

---
+++ utils/themes.tsx
@@ -0,0 +1,9 @@
+export const themeSelectOptions = [
+  { id: 0, theme: "#277C78", value: "Green" },
+  { id: 1, theme: "#82C9D7", value: "Cyan" },
+  { id: 2, theme: "#F2CDAC", value: "Yellow" },
+  { id: 3, theme: "#626070", value: "Navy" },
+  { id: 4, theme: "#826CB0", value: "Purple" },
+  { id: 5, theme: "#000000", value: "Black" },
+  { id: 6, theme: "#c94736", value: "Red" },
+];
