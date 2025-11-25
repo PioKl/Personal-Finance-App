1e9d9dfd930a1876ab992e9f83a9175dbcffba41
PioKl <kosiu342@gmail.com>
2025-11-25 19:21:12 +0100

    Add pots with Add New Pot button and AddEditModal

--- app/(root)/pots/page.tsx
+++ app/(root)/pots/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import { useState } from "react";
+import Button from "@/components/ui/Button";
+import AddEditModal from "@/components/modals/AddEditModal";
+
 const Pots = () => {
-  return <h1>Pots</h1>;
+  const [openAddModal, setOpenAddModal] = useState(false);
+  return (
+    <section className="grid gap-space-400">
+      <div className="flex items-center justify-between">
+        <h1>Pots</h1>
+        <Button
+          variant="primary"
+          isALink={false}
+          className="flex items-center gap-space-150"
+          onClick={() => {
+            setOpenAddModal(true);
+          }}
+        >
+          + Add New Pot
+        </Button>
+        <AddEditModal
+          open={openAddModal}
+          onClose={() => setOpenAddModal(false)}
+          category="Pot"
+          variant="add"
+          message="Create a pot to set savings targets. These can help keep you on track as you save for special purchases."
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Pots;
