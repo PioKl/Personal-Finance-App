7939656d74b90749c1f9a597c3d35412b99233fd
PioKl <kosiu342@gmail.com>
2025-11-28 12:36:05 +0100

    Add formatPercent function

--- utils/formattingFunctions.tsx
+++ utils/formattingFunctions.tsx
@@ -42,3 +42,16 @@ export function formatAmount(amount: number): string {
     })
   );
 }
+
+//Procenty
+export function formatPercent(value: number) {
+  const fixed = value.toFixed(2); //zaokrąglenie do dwóch miejsc po przecinku np. "66.66"
+  const [int, dec] = fixed.split("."); // ["66", "66"] np jak jest 7.95 to in jest 7 a dec 95, 66.66 to właśnie int 66, a dec 66
+
+  // jeśli dwie cyfry po przecinku są takie same → skróć, czyli z 66.66 zrobi 66.6
+  if (dec[0] === dec[1]) {
+    return `${int}.${dec[0]}`;
+  }
+
+  return `${int}.${dec}`; //w innym przypadku normalnie połączy czyli 7 i 95 to będzie 7.95
+}
