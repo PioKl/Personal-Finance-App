e1dcaa65f59bfb9bf840610c6100454b4c16ce81
PioKl <kosiu342@gmail.com>
2025-12-04 19:42:12 +0100

    Add AddWithdrawMoneyModal (in progress)

---
+++ components/modals/pots/AddWithdrawMoneyModal.tsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import { AddWithdrawMoneyModal as AddWithdrawMoneyModalType } from "@/types";
+import { Modal, Box } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconCloseModal from "@/assets/icons/icon-close-modal.svg";
+import {
+  priceDollarsFormatting,
+  formatPercent,
+} from "@/utils/formattingFunctions";
+import clsx from "clsx";
+import { isDigit } from "@/utils/validations";
+import Button from "@/components/ui/Button";
+
+export default function AddWithdrawMoneyModal({
+  variant,
+  message,
+  amount,
+  target,
+  name,
+  open,
+  onClose,
+}: AddWithdrawMoneyModalType) {
+  const amountSaved = (amount / target) * 100;
+  //Walidacja, errory
+  const [spendingAmount, setSpendingAmount] = useState("");
+  const validation = {
+    amount: spendingAmount.trim() === "", //jeśli puste pole to błąd
+  };
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      <Box className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid gap-space-250 max-w-[335px] px-space-250 py-space-300 custom-shadow-modal bg-fill-two rounded-default md:max-w-[560px] md:p-space-400">
+        <div className="flex items-center justify-between">
+          <h1
+            id="modal-title"
+            /* nadpisanie domyślnych stylów dla h1, które są zdefiniowane w typography */
+            className="!text-preset-2 !tracking-preset-2 !leading-preset-2 md:!text-preset-1 md:!tracking-preset-1 md:!leading-preset-1"
+          >
+            {variant === "add" ? `Add New Pot` : `Withdraw from '${name}'`}
+          </h1>
+          <button onClick={onClose}>
+            <IconCloseModal />
+          </button>
+        </div>
+        <p>{message}</p>
+        <div className="grid gap-space-200">
+          <div className="flex items-center justify-between">
+            <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4 text-color-three">
+              New Amount
+            </span>
+            <span className="text-preset-1 tracking-preset-1 leading-preset-1 font-preset-1 text-color-one">
+              ${priceDollarsFormatting(amount)}
+            </span>
+          </div>
+          <div className="relative w-full h-2 bg-fill-three rounded-small">
+            <div
+              className="absolute w-[calc(var(--spent-percent)-8px)] h-2 bg-fill-one rounded-small"
+              style={
+                {
+                  width: `clamp(4px, ${Math.min(amountSaved, 100)}%, 100%)`,
+                } as React.CSSProperties
+              }
+            ></div>
+            {/* Math.min, druga wartość tam gdzie 100 oznacza ograniczenie do 100 tylko max tyle może osiągnać */}
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-preset-5 tracking-preset-5 leading-preset-5 font-preset-5-bold text-color-three">
+              {formatPercent(amountSaved)}%
+            </span>
+            <span className="text-preset-5 tracking-preset-5 leading-preset-5 font-preset-5 text-color-three">
+              Target of ${target}
+            </span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-start gap-space-100">
+          <span className="flex text-preset-5 tracking-preset-5 leading-preset-5 font-preset-5-bold text-color-three whitespace-nowrap">
+            Amount to Add
+          </span>
+          <FormControl
+            className={clsx("mui-select w-full text-left")}
+            error={validation.amount}
+          >
+            <OutlinedInput
+              value={spendingAmount}
+              onChange={(e) => {
+                if (isDigit(e.target.value)) {
+                  setSpendingAmount(e.target.value);
+                }
+              }}
+              placeholder="e.g. 2000"
+              label="" // ← konieczne, żeby placeholder był widoczny
+              classes={{
+                input:
+                  "!py-space-150 !text-preset-4 !tracking-preset-4 !leading-preset-4 !font-preset-4", // ← padding inputa
+              }}
+              sx={{
+                "& input::placeholder": {
+                  color: "#98908b", // kolor placeholdera
+                  opacity: 1,
+                },
+              }}
+              startAdornment={
+                <InputAdornment position="start">
+                  <span className="text-preset-4 tracking-preset-4 leading-preset-4 font-preset-4 text-input-placeholder">
+                    $
+                  </span>
+                </InputAdornment>
+              }
+              notched={false}
+            />
+          </FormControl>
+        </div>
+        {variant === "add" ? (
+          <Button variant="primary" isALink={false}>
+            Confirm Addition
+          </Button>
+        ) : (
+          <Button variant="primary" isALink={false}>
+            Confirm Withdrawal
+          </Button>
+        )}
+      </Box>
+    </Modal>
+  );
+}
